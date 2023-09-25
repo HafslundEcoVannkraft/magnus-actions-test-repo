20d7fe0fa7fdf4aeb62394b7ef1c7b95246047cc
Magnus Longva <magnus@longva.email>
2023-09-25 12:56:26 +0200

    debug

--- lib.py
+++ lib.py
@@ -3,5 +3,7 @@ import sys
 
 def print_file_path():
     print("lib.py: print_file_path()")
-    print(os.path.realpath(__file__))
-    print(' '.join([s for s in sys.argv]))
+    file_path = sys.argv[0]
+    dirname = os.environ['GITHUB_WORKSPACE']
+    relative_file_path = os.path.relpath(file_path, dirname)
+    print(f"https://github.com/{os.environ['GITHUB_REPOSITORY']}/blob/{os.environ['branch']}/{relative_file_path}")
--- main.py
+++ main.py
@@ -6,4 +6,4 @@ from lib import print_file_path
 print(os.path.realpath(__file__))
 print(' '.join([s for s in sys.argv]))
 print_file_path()
-print(f"https://github.com/{os.environ['GITHUB_REPOSITORY']}/blob/{os.environ['branch']}/")
+#print(f"https://github.com/{os.environ['GITHUB_REPOSITORY']}/blob/{os.environ['branch']}/")
