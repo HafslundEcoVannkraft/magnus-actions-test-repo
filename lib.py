2961d2abd40ed33e553fab94a8fa0cb3965b0fbf
Magnus Longva <magnus@longva.email>
2023-09-25 12:48:38 +0200

    debug

---
+++ lib.py
@@ -0,0 +1,4 @@
+def print_file_path():
+    print("lib.py: print_file_path()")
+    print(os.path.realpath(__file__))
+    print(' '.join([s for s in sys.argv]))
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #!/bin/python
 import os
 import sys
+from lib import print_file_path
 
 print(os.path.realpath(__file__))
 print(' '.join([s for s in sys.argv]))
+print_file_path()
 print(f"https://github.com/{os.environ['GITHUB_REPOSITORY']}/blob/{os.environ['branch']}/")
