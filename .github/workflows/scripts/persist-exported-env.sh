a95dba37fbae6456088ba56eff7625546142d04f
Magnus Longva <magnus@longva.email>
2023-10-18 11:50:50 +0200

    test script

---
+++ .github/workflows/scripts/persist-exported-env.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export | awk '{$1=$2=""; print substr($0, 3)}' > /tmp/env_vars
+while read -r env_var_declaration
+do
+    echo "${env_var_declaration}" >> $GITHUB_ENV
+done < /tmp/env_vars
