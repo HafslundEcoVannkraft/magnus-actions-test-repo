e67acc1e9ba82e29f7ac6dcebbe4caf3caa4cce8
Magnus Longva <magnus@longva.email>
2023-10-18 12:04:11 +0200

    move grep

--- .github/workflows/scripts/persist-exported-env.sh
+++ .github/workflows/scripts/persist-exported-env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-export | awk '{$1=$2=""; print substr($0, 3)}' > /tmp/env_vars
+export | awk '{$1=$2=""; print substr($0, 3)}' | grep -vE '^[A-Z]+=*' > /tmp/env_vars
 while read -r env_var_declaration
 do
     echo "${env_var_declaration}"
     echo "${env_var_declaration}" | grep "=" >> $GITHUB_ENV
-done < $(grep -vE '^[A-Z]+=*' /tmp/env_vars)
+done < /tmp/env_vars
