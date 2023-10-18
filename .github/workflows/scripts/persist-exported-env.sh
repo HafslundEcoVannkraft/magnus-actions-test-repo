386391725ffecef91dd57752342ca38ba234708e
Magnus Longva <magnus@longva.email>
2023-10-18 13:12:35 +0200

    test GITHUB_ENV properties

--- .github/workflows/scripts/persist-exported-env.sh
+++ .github/workflows/scripts/persist-exported-env.sh
@@ -2,6 +2,5 @@
 export | awk '{$1=$2=""; print substr($0, 3)}' | grep -vE '^[A-Z]+=*' > /tmp/env_vars
 while read -r env_var_declaration
 do
-    echo "${env_var_declaration}"
     echo "${env_var_declaration}" | grep "=" >> $GITHUB_ENV
 done < /tmp/env_vars
