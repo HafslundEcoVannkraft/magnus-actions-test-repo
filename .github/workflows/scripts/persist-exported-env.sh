65dfb2596ca2aabcd8ff1cce813911849d687d3f
Magnus Longva <magnus@longva.email>
2023-10-18 11:58:34 +0200

    only non-empty assignments

--- .github/workflows/scripts/persist-exported-env.sh
+++ .github/workflows/scripts/persist-exported-env.sh
@@ -3,5 +3,5 @@ export | awk '{$1=$2=""; print substr($0, 3)}' > /tmp/env_vars
 while read -r env_var_declaration
 do
     echo "${env_var_declaration}"
-    echo "${env_var_declaration}" >> $GITHUB_ENV
+    echo "${env_var_declaration}" | grep "=" >> $GITHUB_ENV
 done < /tmp/env_vars
