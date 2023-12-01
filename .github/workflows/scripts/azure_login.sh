5422763ff601ac4c73efd2c87eda1275ea17e679
Magnus Longva <magnus@longva.email>
2023-12-01 15:00:55 +0100

    federated token command

--- .github/workflows/scripts/azure_login.sh
+++ .github/workflows/scripts/azure_login.sh
@@ -5,14 +5,20 @@
 # loop or other flow control structures.
 
 oidc_token=$(curl -H "Authorization: bearer $ACTIONS_ID_TOKEN_REQUEST_TOKEN" "$ACTIONS_ID_TOKEN_REQUEST_URL&audience=api://AzureADTokenExchange")
-url="https://login.microsoftonline.com/${tenant_id}/oauth2/v2.0/token"
+az login --service-principal --username 189ee463-4982-4960-a6de-c46d9c4397a8 --tenant 18704e99-efc6-4fc7-9f7d-529190c7a11a --federated-token $oidc_token --allow-no-subscriptions
+az resource show --ids /subscriptions/142a6373-05c8-4f0c-b84c-66d3100aa6bc/resourceGroups/scope1
 
-# we want scopes likes this
-# "scp": "AuditLog.Read.All Directory.AccessAsUser.All email Group.ReadWrite.All openid profile User.ReadWrite.All"
-scopes="https://graph.microsoft.com/.default"
-body="client_id=${client_id}&scope=${scope}&grant_type=urn:ietf:params:oauth:grant-type:jwt-bearer&client_assertion_type=urn:ietf:params:oauth:client-assertion-type:jwt-bearer&client_assertion=${oidc_token}"
-response=$(curl -X POST -d "$body" -H "Content-Type: application/x-www-form-urlencoded" $url --fail --verbose)
-echo "response: $response"
+# url="https://login.microsoftonline.com/${tenant_id}/oauth2/v2.0/token"
+
+# # we want scopes likes this
+# # "scp": "AuditLog.Read.All Directory.AccessAsUser.All email Group.ReadWrite.All openid profile User.ReadWrite.All"
+# scopes="https://graph.microsoft.com/.default"
+# body="client_id=${client_id}&scope=${scope}&grant_type=urn:ietf:params:oauth:grant-type:jwt-bearer&client_assertion_type=urn:ietf:params:oauth:client-assertion-type:jwt-bearer&client_assertion=${oidc_token}"
+# response=$(curl -X POST -d "$body" -H "Content-Type: application/x-www-form-urlencoded" $url --fail --verbose)
+# echo "response: $response"
+
+# 
+
 
 # # 3 - Use the GitHub JWT as proof for authenticating as the app defined in env.CLIENT_ID:
 #               $uri = "https://login.microsoftonline.com/{0}/oauth2/v2.0/token" -f "${{env.TENANT_ID}}"
